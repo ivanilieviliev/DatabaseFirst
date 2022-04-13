5e9a7f403826e0593996791acf402f1e9a0360b3
ivanilieviliev <103572564+ivanilieviliev@users.noreply.github.com>
2022-04-13 16:24:43 +0300

    Add files via upload
    Added Service Layer

---
+++ ServiceLayer/CustomerManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer;
+using DataLayer;
+
+namespace ServiceLayer
+{
+    public class CustomerManager : IContext<Customer, int>
+    {
+        private CustomerContext _customerContext;
+
+
+        public CustomerManager(OnlineShopDBContext context)
+        {
+            this._customerContext = new CustomerContext(context);
+        }
+
+        public void Create(Customer customer)
+        {
+            try
+            {
+                _customerContext.Create(customer);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Customer Read(int key)
+        {
+            try
+            {
+                return _customerContext.Read(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Customer> ReadAll()
+        {
+            try
+            {
+                return _customerContext.ReadAll();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(Customer item)
+        {
+            try
+            {
+                _customerContext.Update(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int key)
+        {
+            try
+            {
+                _customerContext.Delete(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ServiceLayer/DBContextManager.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer
+{
+    public static class DBContextManager
+    {
+        private static OnlineShopDBContext _context;
+
+        static DBContextManager()
+        {
+            _context = new OnlineShopDBContext();
+        }
+
+        public static OnlineShopDBContext CreateContext()
+        {
+            _context = new OnlineShopDBContext();
+            return _context;
+        }
+
+        public static OnlineShopDBContext GetContext()
+        {
+            return _context;
+        }
+
+        public static void SetAutoDetectChanges(bool value)
+        {
+            _context.ChangeTracker.AutoDetectChangesEnabled = value;
+        }
+
+    }
+}
